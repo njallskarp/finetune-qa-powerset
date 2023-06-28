d53f39664455b3b3424ce3f3a11532c12c09fcf1
Lsig <logis21@ru.is>
2023-06-28 09:26:24 +0000

    Changed function name to get_device, much clearer

--- training/utils.py
+++ training/utils.py
@@ -4,14 +4,14 @@ from torch.utils.data import DataLoader
 from transformers import BertForQuestionAnswering
 
 
-def get_rediction() -> device:
+def get_device() -> device:
     return torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 def get_prediction(model: BertForQuestionAnswering, batch: DataLoader[tuple[torch.Tensor, torch.Tensor]]) -> tuple[torch.FloatTensor]: 
   """
   Get the answer prediction from the model
   """
-  device = get_rediction()
+  device = get_device()
 
   input_ids = batch['input_ids'].to(device)
   a_mask = batch['attention_mask'].to(device)
