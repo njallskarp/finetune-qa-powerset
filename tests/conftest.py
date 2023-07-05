f02504308d8dbe5edd49cb519933995a21fab860
Eysteinn Örn Jónsson <eysi@live.com>
2023-07-05 15:19:09 +0000

    adjusting workflow

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,8 @@
-
 import os
 
 import pytest
 import torch
 
 requires_cuda = pytest.mark.skipif(
-    torch.cuda.is_available() or os.environ.get('CI') == 'true',
-    reason="Requires CUDA"
+    torch.cuda.is_available() or os.environ.get("CI") == "true", reason="Requires CUDA"
 )
